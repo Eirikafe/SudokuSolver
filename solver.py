c1a33ee7b81933a674b25cfc935abbeaf07ce89d
Jeremy Bunag <eirikafe@hotmail.com>
2021-03-04 14:12:21 +0800

    Final w/ GUI implementation

---
+++ solver.py
@@ -0,0 +1,88 @@
+import pygame
+
+# hard coded board for testing purposes. 0 represents a blank square
+"""
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+] """
+
+
+# method used to find an empty square and fill in a # 1-9
+# then check if it is valid
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+    return False
+
+
+def valid(bo, num, pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    boxX = pos[1] // 3
+    boxY = pos[0] // 3
+
+    for i in range(boxY * 3, boxY * 3 + 3):
+        for j in range(boxX * 3, boxX * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+# Algorithm used to divide up the board into 3x3 squares with 3x3 numbers
+# Also prints out borders for ease of reading
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - -")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+# method used to find an empty square on the board (a "0")
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return i, j
+
+
+# print_board(board)
+# solve(board)
+# print("________________________")
+# print("     Below is Solved")
+# print("________________________")
+# print_board(board)
